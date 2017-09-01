3892829947adaf404cb11129f57032a99b134148
Mike Pettman <mp_palmatum@yahoo.com>
2017-09-01 17:41:49 -0500

    second part done

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 console.log('js');
 $(document).ready(onReady);
 
+var rCounter = 0;
+var yCounter = 0;
+var gCounter = 0;
+var bCounter = 0;
+
 function onReady(){
     console.log('jQuery has been activated!');
-    //event listeners
     $('.color-button').on('click', cubeCreate);
 
 }
-//function that gets called through as an event listener
+
 function cubeCreate() {
     var $color = $(this).data('color');
-    //We're adding a new div with the "color-cube" class dynamically into the DOM
     $('.container').append($('<div class=color-cube>'));
-    
     $('.color-cube').last().addClass($color);
-    console.log(cubeCreate);
+    if ($color === 'red') {                 // color counters
+        rCounter++;
+        $('#red').html(rCounter);
+    } else if ($color === 'yellow') {
+        yCounter++;
+        $('#yellow').html(yCounter);
+    } else if ($color === 'green') {
+        gCounter++;
+        $('#green').html(gCounter);
+    } else if ($color === 'blue') {
+        bCounter++;
+        $('#blue').html(bCounter);
+    }
 }
