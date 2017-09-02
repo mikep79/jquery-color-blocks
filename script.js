a87cf18f9404047f6b2b915a1c2b007535e5702c
Mike Pettman <mp_palmatum@yahoo.com>
2017-09-02 12:33:34 -0500

    hard mode completed

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ var bCounter = 0;
 function onReady(){
     console.log('jQuery has been activated!');
     $('.color-button').on('click', cubeCreate);
+    $('body').on('click', '.color-cube', destroyCube);
 
 }
 
 function cubeCreate() {
-    var $color = $(this).data('color');
-    $('.container').append($('<div class=color-cube>'));
-    $('.color-cube').last().addClass($color);
-    if ($color === 'red') {                 // color counters
+    console.log('created!');    
+    var $color = $(this).data('color');                     // extract color data
+    $('.container').append($('<div class=color-cube>'));    // create new div box
+    $('.color-cube').last().addClass($color);               // add color class
+    if ($color === 'red') {                                 // change color counters
         rCounter++;
         $('#red').html(rCounter);
     } else if ($color === 'yellow') {
@@ -27,6 +29,24 @@ function cubeCreate() {
         $('#green').html(gCounter);
     } else if ($color === 'blue') {
         bCounter++;
+        $('#blue').html(bCounter);
+    }
+}
+
+function destroyCube() {
+    console.log('destroyed!');
+    $(this).remove();                                   // destroy div
+    if ($(this).hasClass('red')) {                      // deterime color class of div
+        rCounter--;                                     // lower counter
+        $('#red').html(rCounter);                       // update counters
+    } else if ($(this).hasClass('yellow')) {
+        yCounter--;
+        $('#yellow').html(yCounter);
+    } else if ($(this).hasClass('green')) {
+        gCounter--;
+        $('#green').html(gCounter);
+    } else if ($(this).hasClass('blue')) {
+        bCounter--;
         $('#blue').html(bCounter);
     }
 }
